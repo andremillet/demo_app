a67d971498314ff16d7519717a17ab985f976fb8
André Millet <andremillet@gmail.com>
2011-03-04 16:17:14 -0300

    versao um

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Supernova36::Application.config.secret_token = 'fd62989540ea1a07ba1b6b7aad304b7feba91b782244cdf30b9e07c69926fe6346044df45c786d84309c339bf7042a02d2dd20d984d309c31f7278fd562148c9'
